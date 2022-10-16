814c6f34d07ca61b47ab53e8f0106cad8894f697
shaikh sufyan <shaikhsufyan1811@gmail.com>
2022-10-16 09:25:30 +0530

    Basic-project

---
+++ AddArray.js
@@ -0,0 +1,7 @@
+// Add if Ethiopia there it not present in array. if present then print it 
+
+let country=["india", "Dublin", "Canada", "Dubai"];
+country.push("Ethiopia");
+console.log(country);
+ 
+console.log(country[4]);
