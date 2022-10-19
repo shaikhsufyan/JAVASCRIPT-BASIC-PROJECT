d2d7bc0700e407faa240e15d084f366a9a3f64f3
shaikh sufyan <shaikhsufyan1811@gmail.com>
2022-10-19 10:51:16 +0530

    Find Area of Traingle

---
+++ AreaOfTraingle/AreaTriangle.js
@@ -0,0 +1,7 @@
+//Calculate Area of Traingle
+
+let base=prompt("Enter the value of Base");
+let height=prompt("Enter the value of height");
+let area=base*height/2;
+document.getElementById("value").innerHTML="Base :"+base+" Height :"+height+ "  Area of Traingle is "+ ": "+area;
+console.log("Base :"+base+" Height :"+height+ "  Area of Traingle is "+ ": "+area)
